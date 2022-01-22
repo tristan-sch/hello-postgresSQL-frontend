4b0d77ba34c1f8c9cde7c89e1629cf0f12df9d58
Tristan <tristan@Tristans-iMac.local>
2022-01-22 16:19:07 +0000

    updated MakeBlog.js and Readme.md

--- src/components/MakeBlog.js
+++ src/components/MakeBlog.js
@@ -2,9 +2,11 @@ import { useState } from "react";
 
 const MakeBlog = () => {
   const [text, setText] = useState("");
+  const [oldTitle, setOldTitle] = useState("");
   const [title, setTitle] = useState("");
   const submitBlog = (e) => {
     const bodyData = {
+      oldTitle,
       title,
       text,
     };
@@ -16,10 +18,42 @@ const MakeBlog = () => {
       .then((r) => r.json())
       .then(console.log);
   };
+  const updateBlog = (e) => {
+    const bodyData = {
+      oldTitle,
+      title,
+      text,
+    };
+    fetch("http://localhost:5001/p/blog", {
+      method: "PUT",
+      body: JSON.stringify(bodyData),
+      headers: { "content-type": "application/json" },
+    })
+      .then((r) => r.json())
+      .then(console.log);
+  };
+  const deleteBlog = (e) => {
+    const bodyData = {
+      title,
+    };
+    fetch("http://localhost:5001/p/blog", {
+      method: "DELETE",
+      body: JSON.stringify(bodyData),
+      headers: { "content-type": "application/json" },
+    })
+      .then((r) => r.json())
+      .then(console.log);
+  };
   return (
     <div>
       Make a blog:
       <br />
+      Old title:
+      <input
+        value={oldTitle}
+        onChange={(e) => setOldTitle(e.target.value)}
+        type="text"
+      ></input>
       Title:
       <input
         value={title}
@@ -33,6 +67,8 @@ const MakeBlog = () => {
         onChange={(e) => setText(e.target.value)}
       ></textarea>
       <button onClick={submitBlog}>Submit your post</button>
+      <button onClick={updateBlog}>Update this post</button>
+      <button onClick={deleteBlog}>Delete this post</button>
     </div>
   );
 };
